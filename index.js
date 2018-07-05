3be35e0c1ff86dd11c783bcd7c05aeaa05fc4196
edkassin18@gmail.com <edkassin18@gmail.com>
2018-07-05 01:18:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string){
-  if (string.toLowerCase()) === string{
+  if (string.toLowerCase() === string{
     return "I can\'t hear you!"
   }
-   if (string.toUpperCase()) === string){
+  else if (string.toUpperCase() === string){
     return "YES INDEED!"
   }
-   if (string === "I love you, Grandma."){
+  else if (string === "I love you, Grandma."){
     return "I love you, too."
   }
   else return "This is great!"
