4d28420ae100087265ba834b95b85a7798e14f24
edkassin18@gmail.com <edkassin18@gmail.com>
2018-07-05 01:12:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function sayHiToGrandma(string){
   if (string.toLowerCase()) === string{
     return "I can\'t hear you!"
   }
-  else if (string.toUpperCase()=== string){
+  else if (string.toUpperCase())=== string){
     return "YES INDEED!"
   }
   else (string === "I love you, Grandma."){
