fefa610b01afc28cad0785de3373d0bb0bbf452d
edkassin18@gmail.com <edkassin18@gmail.com>
2018-07-05 01:02:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,9 +7,12 @@ function whisper(string){
 function logShout(string){
   
 }
-  function logShout(string) {
+function logShout(string) {
   console.log(string.toUpperCase())
 }
 function logWhisper(string){
   console.log(string.toLowerCase())
+}
+function SayHiToGrandma(string){
+  
 }
