6134a8fff2b2b09e9ee0afb06b813d5e8d843df3
edkassin18@gmail.com <edkassin18@gmail.com>
2018-07-05 00:57:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function whisper(string){
 function logShout(string){
   
 }
-function logShout(string){
-  console.log(expect(spy).toUpperCase)
+  function logShout(string) {
+  console.log(string.toUpperCase())
 }
